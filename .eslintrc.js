602168ae48335a2b6137f58596b0129212c057d6
Aeero <lhf_personal@126.com>
2017-01-08 22:29:50 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "airbnb",
+    "rules": {
+
+    },
+    "plugins":[
+      "jsx-a11y",
+      "react"
+    ]
+};
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# eslint-explain-with-react
+基于React+Webpack+ES6的前端项目模版目录
